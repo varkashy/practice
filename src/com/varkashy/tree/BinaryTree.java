891d01ef9df6122e4a1acff0448a784f8310cc57
varkashy <varunkkashyap@gmail.com>
2020-03-29 12:30:58 -0500

    BST

---
+++ src/com/varkashy/tree/BinaryTree.java
@@ -0,0 +1,132 @@
+package com.varkashy.tree;
+
+public class BinaryTree {
+    int data;
+    BinaryTree leftNode;
+    BinaryTree rightNode;
+
+    public BinaryTree(int data){
+        this.data = data;
+    }
+
+    public static void inOrderTraversal(BinaryTree root){
+        if(root == null){
+            return;
+        }
+        inOrderTraversal(root.leftNode);
+        System.out.println(root.data);
+        inOrderTraversal(root.rightNode);
+    }
+
+    public static void preOrderTraversal(BinaryTree root){
+        if(root == null){
+            return;
+        }
+        System.out.println(root.data);
+        preOrderTraversal(root.leftNode);
+        preOrderTraversal(root.rightNode);
+    }
+
+    public static void postOrderTraversal(BinaryTree root){
+        if(root == null){
+            return;
+        }
+        postOrderTraversal(root.leftNode);
+        postOrderTraversal(root.rightNode);
+        System.out.println(root.data);
+    }
+
+    public static BinaryTree createBinaryTreeFromArray(int[] numbers){
+        if(numbers.length == 0){
+            return null;
+        }
+        //find midpoint
+        return createBinaryTreeFromArray(numbers,0,numbers.length-1);
+    }
+
+    private static BinaryTree createBinaryTreeFromArray(int[] numbers, int low, int high) {
+        if(low>high){
+            return null;
+        }
+        int mid = (low+high)/2;
+        BinaryTree root = new BinaryTree(numbers[mid]);
+        root.leftNode = createBinaryTreeFromArray(numbers,low,mid-1);
+        root.rightNode = createBinaryTreeFromArray(numbers,mid+1,high);
+        return root;
+    }
+
+    public static BinaryTree insertIntoBinaryTree(BinaryTree root, int data){
+        if(root == null){
+            root = new BinaryTree(data);
+        }
+        else if(data>root.data){
+            root.rightNode = insertIntoBinaryTree(root.rightNode,data);
+        }
+        else if (data < root.data){
+            root.leftNode = insertIntoBinaryTree(root.leftNode,data);
+        }
+        return root;
+    }
+
+    public static BinaryTree deleteFromBinaryTree(BinaryTree root,int data){
+        /*
+        Three cases are possible
+        Node to be deleted has no children -> mark node as null
+        Node to be deleted has only one child -> left or right -> replace node with left or right child
+        Node to be deleted has both child -> go to the right subtree -> find the left most node ->
+        replace root with left most node of the right subtree -> delete left most node of the right subtree
+         */
+        if(root == null){
+            return root;
+        }
+        if(root.data == data){
+            if(root.leftNode == null && root.rightNode == null){
+                return null;
+            }
+            else if(root.leftNode == null || root.rightNode == null ){
+                if(root.leftNode == null){
+                    root = root.rightNode;
+                    return root;
+                }
+                if(root.rightNode == null){
+                    root = root.leftNode;
+                    return root;
+                }
+            }
+            else {
+                BinaryTree minimumNode = findMinimumNode(root.rightNode);
+                root.data = minimumNode.data;
+                deleteFromBinaryTree(root.rightNode,minimumNode.data);
+                return root;
+            }
+        }
+        if(root.data < data){
+            root.rightNode = deleteFromBinaryTree(root.rightNode,data);
+        }
+        if(root.data > data){
+            root.leftNode = deleteFromBinaryTree(root.leftNode,data);
+        }
+        return root;
+    }
+
+    public static BinaryTree searchInBinaryTree(BinaryTree root, int data){
+        if(root == null){
+            return null;
+        }
+        if(data == root.data){
+            return root;
+        }
+        if(data>root.data){
+           return  searchInBinaryTree(root.rightNode,data);
+        }
+        else{
+          return  searchInBinaryTree(root.leftNode,data);
+        }
+    }
+    private static BinaryTree findMinimumNode(BinaryTree root) {
+        while(root.leftNode!=null){
+            root = root.leftNode;
+        }
+        return root;
+    }
+}
---
+++ src/com/varkashy/tree/test/TestBinaryTree.java
@@ -0,0 +1,49 @@
+package com.varkashy.tree.test;
+
+import com.varkashy.tree.BinaryTree;
+
+public class TestBinaryTree {
+    public static void main(String[] args) {
+        BinaryTree root = BinaryTree.createBinaryTreeFromArray(new int[]{1, 2, 3, 4, 5, 6, 7});
+        testTraversal(root);
+        testSearch(root,new int[]{3,11});
+        testInsertIntoBinaryTree(root, new int[]{-1,11});
+        testDeleteFromBinaryTree(root, new int[]{4,7,11});
+    }
+
+    private static void testDeleteFromBinaryTree(BinaryTree root, int[] ints) {
+        for(int toDelete:ints){
+            root = BinaryTree.deleteFromBinaryTree(root,toDelete);
+            System.out.println("inOrder after deleting "+toDelete);
+            BinaryTree.inOrderTraversal(root);
+        }
+    }
+
+    private static void testInsertIntoBinaryTree(BinaryTree root, int[] ints) {
+        for(int toInsert: ints){
+           root = BinaryTree.insertIntoBinaryTree(root,toInsert);
+        }
+        BinaryTree.inOrderTraversal(root);
+    }
+
+    private static void testSearch(BinaryTree root, int[] ints) {
+        for(int toSearch:ints){
+            BinaryTree resultNode = BinaryTree.searchInBinaryTree(root,toSearch);
+            if(resultNode == null){
+                System.out.println("Not Found");
+            }
+            else{
+                System.out.println("Found");
+            }
+        }
+    }
+
+    private static void testTraversal(BinaryTree root) {
+        System.out.println("In-Order");
+        BinaryTree.inOrderTraversal(root);
+        System.out.println("Post-Order");
+        BinaryTree.postOrderTraversal(root);
+        System.out.println("Pre-Order");
+        BinaryTree.preOrderTraversal(root);
+    }
+}
